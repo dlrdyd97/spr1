52fa70143b00d7645fed4b420b4d1ae2595db1c5
dlrdyd97 <dlrdyd97@naver.com>
2024-04-02 13:19:17 +0900

    초기

---
+++ src/main/java/spr1/Spr1Application.java
@@ -0,0 +1,13 @@
+package spr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spr1Application.class, args);
+	}
+
+}
---
+++ src/test/java/spr1/Spr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package spr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spr1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
