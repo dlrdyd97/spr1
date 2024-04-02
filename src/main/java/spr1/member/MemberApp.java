68eb1fb7d1cddf3e219053a6403f6bd42776e50d
dlrdyd97 <dlrdyd97@naver.com>
2024-04-02 14:41:31 +0900

    Test

---
+++ src/main/java/spr1/member/MemberApp.java
@@ -0,0 +1,14 @@
+package spr1.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "TaeHyuck", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+
+    }
+}
